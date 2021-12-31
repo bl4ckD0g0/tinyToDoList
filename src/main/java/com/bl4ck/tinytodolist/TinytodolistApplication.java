a62a2ede40de914adcabe442a934a1ea3657b174
bl4ckD0g0 <96089284+bl4ckD0g0@users.noreply.github.com>
2021-12-31 17:42:00 +0100

    Generada estructura proyecto spring

---
+++ src/main/java/com/bl4ck/tinytodolist/TinytodolistApplication.java
@@ -0,0 +1,13 @@
+package com.bl4ck.tinytodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinytodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinytodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bl4ck/tinytodolist/TinytodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl4ck.tinytodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinytodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

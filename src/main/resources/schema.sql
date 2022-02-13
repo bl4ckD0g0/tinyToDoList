6672186b60af5004795c748315a056903393d9d5
Andrés López Soleto <gordilocuente@gmail.com>
2022-02-13 21:24:45 +0100

    Inicializacion de datos en H2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,25 @@
+create table boards (
+	id					INT NOT NULL AUTO_INCREMENT,
+	name 		VARCHAR(50),
+
+	PRIMARY KEY (id)
+);
+
+create table taskLists (
+	id					INT NOT NULL AUTO_INCREMENT,
+	boardId				INT NOT NULL,
+	name 		VARCHAR(50),
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (boardId) REFERENCES boards(id)
+);
+
 create table tasks (
-	name VARCHAR(50),
-	description TEXT
+	id					INT NOT NULL AUTO_INCREMENT,
+	taskListId				INT,
+	name 		VARCHAR(50),
+	description		   TEXT,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (taskListId) REFERENCES taskLists(id)
 );
